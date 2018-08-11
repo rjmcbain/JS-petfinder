6c8edf1d57e41a5817a69a11a8854822ef91216a
rjmcbain <rmcbain01@gmail.com>
2018-08-10 20:39:36 -0600

    working on js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import fetchJsonp from "fetch-jsonp";
+
+const petForm = document.querySelector("#pet-form");
+
+petForm.addEventListener.apply("submit", fetchAnimals);
+
+// Fetch Animals From Api
+function fetchAnimals(e) {
+  e.preventDefault();
+
+  // Get User Input
+  const animal = document.querySelector("#animal").value;
+  const zip = document.querySelector("#zip").value;
+
+  // Fetch Pets
+  fetchJsonp(`http://api.petfinder.com/pet.find`);
+}
